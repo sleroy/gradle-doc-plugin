ba5183b29e94f7a40362220edc76519bc9818c3c
Sylvain Leroy <sleroy0@gmail.com>
2014-08-13 14:07:21 +0200

    [rollback] velocity conflicts with markdown syntax

--- src/main/groovy/com/metrixware/gradle/markdown/CopyResourcesTask.groovy
+++ src/main/groovy/com/metrixware/gradle/markdown/CopyResourcesTask.groovy
@@ -21,17 +21,8 @@ import java.text.SimpleDateFormat
 
 import nz.net.ultraq.lesscss.LessCSSCompiler
 
-import org.apache.commons.io.FileUtils
 import org.apache.tools.ant.filters.ReplaceTokens
-import org.apache.velocity.VelocityContext
-import org.apache.velocity.app.VelocityEngine
-import org.apache.velocity.runtime.log.SystemLogChute
-import org.codehaus.groovy.runtime.DefaultGroovyMethods
 import org.gradle.api.DefaultTask
-import org.gradle.api.GradleException
-import org.gradle.api.file.ConfigurableFileTree
-import org.gradle.api.file.FileVisitDetails
-import org.gradle.api.file.FileVisitor
 import org.gradle.api.tasks.TaskAction
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -79,18 +70,15 @@ class CopyResourcesTask extends DefaultTask {
 		magicVariablesMap.putAll(project.documentation.templateVariables)
 
 
-
 		LOGGER.info('Copy all md resources into a same folder')
 		// Copy all .md files into the same directory
 		project.fileTree(docFolder) { include '**/*.md' }.each { docFile ->
 			project.copy {
-				from docFile
+				from(docFile) {
+					filter(ReplaceTokens, tokens: magicVariablesMap)
+				}
 				into tmpFolder
 			}
-		}
-		if (!project.documentation.templateVariables.isEmpty()) {
-			LOGGER.info('Processing velocity templates...')
-			executeVelocityFilters(project.documentation.velocityFileFilter)
 		}
 
 
@@ -128,58 +116,8 @@ class CopyResourcesTask extends DefaultTask {
 			}
 			into tmpTemplatesFolder
 		}
-	}
 
 
-	void executeVelocityFilters(String velocityFileFilter) {
 
-		def tmpFolder = project.file(project.documentation.folder_tmp)
-		def tmpVMFolder = project.file(project.documentation.folder_tmp + '/vm')
-
-		DefaultGroovyMethods.deleteDir(tmpVMFolder)
-		tmpVMFolder.mkdirs()
-
-		final VelocityEngine engine = new VelocityEngine()
-		engine.setProperty(VelocityEngine.RUNTIME_LOG_LOGSYSTEM_CLASS, SystemLogChute.name)
-		engine.setProperty(VelocityEngine.RESOURCE_LOADER, 'file')
-		engine.setProperty(VelocityEngine.FILE_RESOURCE_LOADER_CACHE, 'true')
-
-		Map<String, Object> inputFilesSpec = new HashMap<String, Object>()
-		inputFilesSpec.put('dir', tmpFolder)
-		inputFilesSpec.put('include', velocityFileFilter)
-		LOGGER.info("Scanning folder $tmpFolder for templates...")
-		ConfigurableFileTree inputFiles = project.fileTree(inputFilesSpec)
-		inputFiles.visit(new FileVisitor() {
-
-					void visitDir(FileVisitDetails fvd) {
-					}
-
-					void visitFile(FileVisitDetails fvd) {
-						try {
-							LOGGER.info("Found template ${fvd.file}")
-							File outputFile = fvd.relativePath.getFile(tmpVMFolder)
-							VelocityContext context = new VelocityContext()
-							for (Map.Entry<String, Object> e : project.documentation.templateVariables.entrySet()) {
-								context.put(e.key, e.value)
-							}
-							FileReader reader = new FileReader(fvd.file)
-							try {
-								outputFile.parentFile.mkdirs()
-								FileWriter writer = new FileWriter(outputFile)
-								try {
-									engine.evaluate(context, writer, fvd.relativePath.toString(), reader)
-								} finally {
-									writer.close()
-								}
-							} finally {
-								reader.close()
-								LOGGER.info("Replacing ${fvd.file} by ${outputFile}.")
-								FileUtils.copyFile(outputFile, fvd.file)
-							}
-						} catch (IOException e) {
-							throw new GradleException('Failed to process $fvd', e)
-						}
-					}
-				})
 	}
 }
